e2ceb66eaa02ec78fb3f52c00df075d89de4bff1
alex24t <alexobbes@hotmail.com>
2019-05-10 14:59:27 +0200

    Opdracht 1 voltooid
    3 Klassen toegevoegd

---
+++ inschrijvingen.sql
@@ -0,0 +1,2 @@
+INSERT INTO klassen (klas_code, slb_code, cohort, opleiding_code)
+VALUES ('lo9e-amo1', 'bkr02', 2019, 'amo'),('lo9e-amo2', 'akr02', 2019, 'amo'),('lo9e-amo3', 'vlt03', 2019, 'amo');
